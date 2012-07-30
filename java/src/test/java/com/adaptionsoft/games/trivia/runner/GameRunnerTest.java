cd31d2348b9ddb204ecf607e94f9ac9389c34fde
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-07-30 23:04:46 +0200

    Ignore the GameRunnerTest for now, otherwise coverage is high with no real meaning.

--- java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -7,6 +7,8 @@ package com.adaptionsoft.games.trivia.runner;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
+
 /**
  *
  * @author Mirko Friedenhagen <mfriedenhagen-at-gmail.com>
@@ -16,6 +18,7 @@ public class GameRunnerTest {
     /**
      * Test of main method, of class GameRunner.
      */
+    @Ignore("Until we have covered all single methods")
     @Test
     public void testMain() {
         String[] args = null;

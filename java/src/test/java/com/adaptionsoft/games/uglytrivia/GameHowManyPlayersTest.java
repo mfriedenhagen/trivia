e4b555410d1cbf097ad46971df672782d6fb0505
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-07-30 22:54:54 +0200

    Add a Junit Rule for collecting System.out.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameHowManyPlayersTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameHowManyPlayersTest.java
@@ -7,11 +7,25 @@ package com.adaptionsoft.games.uglytrivia;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
 /**
  *
  * @author Mirko Friedenhagen <mfriedenhagen-at-gmail.com>
  */
 public class GameHowManyPlayersTest {
+
+    private static final String ADD_TEMPLATE = "%s was added%nThey are player number %s%n";
+
+    @Rule
+    public SystemOutRule systemOutRule = new SystemOutRule();
 
     public static final int MAX_PLAYERS = 5;
 
@@ -20,26 +34,37 @@ public class GameHowManyPlayersTest {
     @Test
     public void checkNoPlayers() {
         assertEquals(0, sut.howManyPlayers());
+        assertEquals("", systemOutRule.toString());
     }
 
     @Test
     public void checkOnePlayer() {
-        sut.add("Hans Wurst");
+        final String name = "Hans Wurst";
+        final StringBuilder out = new StringBuilder(addPlayer(name, 1));
+        sut.add(name);
         assertEquals(1, sut.howManyPlayers());
+        assertEquals(out.toString(), systemOutRule.toString());
     }
 
     @Test
     public void checkFivePlayers() {
+        StringBuilder out = new StringBuilder();
         for (int i = 0; i < MAX_PLAYERS; i++) {
-            sut.add("Hans Wurst");
+            final String name = "Hans Wurst " + i;
+            sut.add(name);
+            out.append(addPlayer(name, i + 1));
         }
         assertEquals(MAX_PLAYERS, sut.howManyPlayers());
+        assertEquals(out.toString(), systemOutRule.toString());
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void checkSixPlayers() {
-        for (int i = 0; i < 6; i++) {
-            sut.add("Hans Wurst");
-        }
+        checkFivePlayers();
+        sut.add("Hans Wurst " + 6);
+    }
+
+    private String addPlayer(final String name, int index) {
+        return String.format(Locale.ENGLISH, ADD_TEMPLATE, name, index);
     }
 }
---
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/SystemOutRule.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adaptionsoft.games.uglytrivia;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.rules.ExternalResource;
+
+/**
+ *
+ * @author Mirko Friedenhagen <mfriedenhagen-at-gmail.com>
+ */
+class SystemOutRule extends ExternalResource {
+
+    private static final String ENCODING = "UTF-8";
+
+    private final PrintStream originalSystemOut = System.out;
+
+    private final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+    public SystemOutRule() {
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        System.setOut(new PrintStream(bos, true, ENCODING));
+    }
+
+    @Override
+    protected void after() {
+        System.setOut(originalSystemOut);
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return new String(bos.toByteArray(), ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

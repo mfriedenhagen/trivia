e7435a4f9d0152f93fdd325e8390581dd1f64963
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-07-30 21:58:38 +0200

    Run main, no tests.

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adaptionsoft.games.trivia.runner;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
+ */
+public class GameRunnerTest {
+
+    /**
+     * Test of main method, of class GameRunner.
+     */
+    @Test
+    public void testMain() {
+        String[] args = null;
+        GameRunner.main(args);
+    }
+}

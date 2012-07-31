ec3749c57f80f225c9bc18563630c0613911bacb
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-07-31 10:19:25 +0200

    Implemented integrative test which reproduces result.txt from the reference folder. Strange enough, the randomSeq.txt has wrong numbers: - randInt(9) may never return 9 (upper limit is exclusive) - Same goes for randInt(5), but here we have to reduce by one as +1 is done in the main method again.

--- java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -15,7 +15,11 @@ import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Collections2;
 import com.google.common.io.Resources;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -23,8 +27,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 /**
- *
+ * Unfortuna
  * @author Mirko Friedenhagen <mfriedenhagen-at-gmail.com>
  */
 @RunWith(PowerMockRunner.class)
@@ -44,10 +49,15 @@ public class GameRunnerTest {
         System.setOut(new PrintStream(bos, true, ENCODING));
     }
 
+    @Before
+    public void parameterizeRandomMock() {
+        when(randomMock.nextInt(5)).thenReturn(5, 5, 5, 0, 2, 0, 5, 3, 0, 1, 3, 5, 0, 1, 2, 3, 1, 2, 0, 0, 5, 0, 5, 5, 2, 3, 5, 3, 3, 1, 5, 3, 1, 4, 4, 3, 5, 3, 0, 5, 4, 4, 4, 4, 0, 1, 2, 2, 3, 3);
+        when(randomMock.nextInt(9)).thenReturn(6, 9, 2, 4, 7, 0, 5, 1, 0, 2, 8, 4, 2, 3, 8, 1, 2, 5, 1, 7, 8, 4, 5, 7, 8, 1, 3, 1, 9, 2, 9, 6, 2, 2, 1, 0, 2, 6, 1, 2, 8, 9, 7, 1, 3, 5, 2, 5, 1, 3);
+    }
+
     @After
     public void restoreSystemOut() throws UnsupportedEncodingException {
         System.setOut(originalSystemOut);
-
     }
 
     /**
@@ -56,16 +66,15 @@ public class GameRunnerTest {
     //@Ignore("Until we have covered all single methods")
     @Test
     public void testMain() throws Exception {
-        final String expectedStdOut = Resources.toString(Resources.getResource(this.getClass(), "GameRunnerTest1.txt"), Charsets.UTF_8);
+        final String expectedStdOut = Resources.toString(Resources.getResource(this.getClass(), "result.txt"), Charsets.UTF_8);
         System.setOut(new PrintStream(bos, true, ENCODING));
         try {
             String[] args = null;
             whenNew(Random.class).withNoArguments().thenReturn(randomMock);
-            when(randomMock.nextInt(5)).thenReturn(4);
             GameRunner.main(args);
         } finally {
             System.setOut(originalSystemOut);
-            final String capturedStdOut = new String(bos.toByteArray(), ENCODING);
+            final String capturedStdOut = new String(bos.toByteArray(), ENCODING).replaceAll("\r\n", "\n");
             assertEquals(expectedStdOut, capturedStdOut);
         }
     }

2edcc588da8d5f3a95a7218d7a0e9695e77cc9d9
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2012-07-31 22:56:44 +0200

    Add tests for overflowing places.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameRollTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameRollTest.java
@@ -81,4 +81,66 @@ public class GameRollTest extends AbstractGameTest {
                 name, name, name, roll, name, name);
         assertEquals(expectedOutput, actualStdOut);
     }
+
+    @Test
+    public void assertOnePlayerWhoHadAnsweredWronglyRollsTwoSixesAndAThree() {
+        final String name = "Hans Wurst";
+        sut.add(name);
+        sut.wrongAnswer();
+        sut.roll(6);
+        sut.roll(6);
+        sut.roll(3);
+        final String actualStdOut = systemOutRule.toString();
+        final String expectedOutput = String.format(Locale.ENGLISH,
+                "%s was added%n" + //
+                "They are player number 1%n" + //
+                "Question was incorrectly answered%n" + //
+                "%s was sent to the penalty box%n" + //
+                "%s is the current player%n" + //
+                "They have rolled a 6%n" + //
+                "%s is not getting out of the penalty box%n" + //
+                "%s is the current player%n" + //
+                "They have rolled a 6%n" + //
+                "%s is not getting out of the penalty box%n" +//
+                "%s is the current player%n" + //
+                "They have rolled a 3%n" + //
+                "%s is getting out of the penalty box%n" + //
+                "%s's new location is 3%n" + //
+                "The category is Rock%n" + //
+                "Rock Question 0%n",
+                name, name, name, name, name, name, name, name, name);
+        assertEquals(expectedOutput, actualStdOut);
+        assertEquals(3, sut.places[0]);
+    }
+    @Test
+    public void assertOnePlayerRollsTwoSixesAndAThree() {
+        final String name = "Hans Wurst";
+        sut.add(name);
+        sut.roll(6);
+        sut.roll(6);
+        sut.roll(3);
+        final String actualStdOut = systemOutRule.toString();
+        final String expectedOutput = String.format(Locale.ENGLISH,
+                "%s was added%n" + //
+                "They are player number 1%n" + //
+                "%s is the current player%n" + //
+                "They have rolled a 6%n" + //
+                "%s's new location is 6%n" + //
+                "The category is Sports%n" + //
+                "Sports Question 0%n" + //
+                "%s is the current player%n" + //
+                "They have rolled a 6%n" + //
+                "%s's new location is 0%n" + //
+                "The category is Pop%n" + //
+                "Pop Question 0%n" + //
+                "%s is the current player%n" + //
+                "They have rolled a 3%n" + //
+                "%s's new location is 3%n" + //
+                "The category is Rock%n" + //
+                "Rock Question 0%n",
+                name, name, name, name, name, name, name);
+        //6 Hans Wurst's new location is 6 The category is Sports Sports Question 0 Hans Wurst is the current player They have rolled a 6 Hans Wurst's new location is 0 The category is Pop Pop Question 0 Hans Wurst is the current player They have rolled a 3 Hans Wurst's new location is 3 The category is Rock Rock] Question 0
+        assertEquals(expectedOutput, actualStdOut);
+        assertEquals(3, sut.places[0]);
+    }
 }

09aea366958bb66dd0989009ec4d14d59507d579
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2014-03-11 22:30:39 +0100

    Explain why we use random.nextInt(5) instead of 6 and decrease all values by 1.
    All diced values are com.adaptionsoft.games.trivia.runner.GameRunner.main() for 5 are
    increased by one afterwards, so we need to adapt the reference values here.

--- java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerIT.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerIT.java
@@ -33,6 +33,8 @@ public class GameRunnerIT {
 
     @Before
     public void parameterizeRandomMock() {
+        // React when dice shows  5 because com.adaptionsoft.games.trivia.runner.GameRunner.main()
+        // uses "random.nextInt(5) + 1". So substract 1 from the reference values for 1-6!
         when(randomMock.nextInt(5)).thenReturn(5, 5, 5, 0, 2, 0, 5, 3, 0, 1, 3, 5, 0, 1, 2, 3, 1, 2, 0, 0, 5, 0, 5, 5, 2, 3, 5, 3, 3, 1, 5, 3, 1, 4, 4, 3, 5, 3, 0, 5, 4, 4, 4, 4, 0, 1, 2, 2, 3, 3);
         when(randomMock.nextInt(9)).thenReturn(6, 9, 2, 4, 7, 0, 5, 1, 0, 2, 8, 4, 2, 3, 8, 1, 2, 5, 1, 7, 8, 4, 5, 7, 8, 1, 3, 1, 9, 2, 9, 6, 2, 2, 1, 0, 2, 6, 1, 2, 8, 9, 7, 1, 3, 5, 2, 5, 1, 3);
     }

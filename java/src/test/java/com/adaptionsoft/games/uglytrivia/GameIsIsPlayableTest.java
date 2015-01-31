3736210b9c4f2d9c266402fe53e081adf68f42a6
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-01-31 21:52:08 +0100

    as must come before any assertions because the description will not be picked up otherwise.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameIsIsPlayableTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameIsIsPlayableTest.java
@@ -16,18 +16,18 @@ public class GameIsIsPlayableTest extends AbstractGameTest {
 
     @Test
     public void assertThatAGameWithNoPlayersIsNotPlayable() {
-        assertThat(sut.isPlayable()).isFalse().as("A game with no players should not be playable.");
+        assertThat(sut.isPlayable()).as("A game with no players should not be playable.").isFalse();
     }
 
     @Test
     public void assertThatAGameWithOnePlayerIsNotPlayable() {
         sut.add("Hans Wurst");
-        assertThat(sut.isPlayable()).isFalse().as("A game with one player should not be playable.");
+        assertThat(sut.isPlayable()).as("A game with one player should not be playable.").isFalse();
     }
     @Test
     public void assertThatAGameWithMoreThanOnePlayerIsPlayable() {
         sut.add("Hans Wurst 1");
         sut.add("Hans Wurst 2");
-        assertThat(sut.isPlayable()).isTrue().as("A game with more than one player should be playable.");
+        assertThat(sut.isPlayable()).as("A game with more than one player should be playable.").isTrue();
     }
 }

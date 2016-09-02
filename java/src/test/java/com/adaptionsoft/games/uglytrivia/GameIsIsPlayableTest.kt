fe98f458e20c685e587e5ff29bb8e1cd8bcfd324
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-09-02 21:34:02 +0200

    Control stdout.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameIsIsPlayableTest.kt
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameIsIsPlayableTest.kt
@@ -14,18 +14,22 @@ class GameIsIsPlayableTest : SystemOutShouldSpec() {
             should("not be playable with no players.") {
                 val sut = Game()
                 sut.isPlayable shouldBe false
+                systemOut.toLines().size shouldBe 1
             }
             should("not be playable with one player.") {
                 val sut = Game()
                 sut.add("Hans Wurst")
                 sut.isPlayable shouldBe false
+                systemOut.toLines().size shouldBe 2
             }
             should("be playable with more than one player.") {
                 val sut = Game()
                 sut.add("Hans Wurst 1")
                 sut.add("Hans Wurst 2")
                 sut.isPlayable shouldBe true
+                systemOut.toLines().size shouldBe 4
             }
         }
+
     }
 }
